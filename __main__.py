8c7b044f19832f439eb074e542f54bee92b71833
pwodyk <14218944+pwodyk@users.noreply.github.com>
2022-01-02 19:46:40 +0000

    created main function, running hashing value on sample file

---
+++ __main__.py
@@ -0,0 +1,15 @@
+import os
+import hashlib
+
+def main():
+    hash_file = open('sample\hashes.txt', 'r')
+    hash_list = hash_file.readlines()
+    sample_file = open('sample\sample.exe', 'rb')
+    sha1 = hashlib.sha1(sample_file.read())
+
+    print(sha1.hexdigest().capitalize())
+    print(hash_list[0])
+
+
+main()
+print("end")
