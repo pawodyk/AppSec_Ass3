7da628380a37741043d0ca3a3fcfdfee345e71a3
pwodyk <14218944+pwodyk@users.noreply.github.com>
2022-01-03 19:03:10 +0000

    added functions scan(dirpath) and readfile(filepath)

--- __main__.py
+++ __main__.py
@@ -3,10 +3,10 @@ import hashlib
 from dotenv import load_dotenv
 
 def main():
-    load_dotenv()
-    VT_API_KEY = os.getenv('VT_API')    
+    scan("D:\\Desktop\\Code\\MalFileDetTool\\sample")
+    pass
     
-    
+
 
 def test():
     hash_file = open('sample\hashes.txt', 'r')
@@ -21,10 +21,31 @@ def test():
     hash_file.close()
 
 
+def scan(dirpath):
+    os.chdir(dirpath)
+    for file in os.listdir():
+        filepath = os.getcwd() + '\\' + file
+        out = readfile(filepath)
+        print(out['name'],out['sha1'])
+
+        
+
+def readfile(filepath):
+
+    out = {}
+    with open(filepath, 'rb') as file:
+        file_sha1 = hashlib.sha1(file.read())
+        pathsplit = os.path.split(filepath)
+        # print('filename: {1}\t located at {0}\t SHA1: {2}'.format(pathsplit[0],pathsplit[1],))  # , file_sha1.hexdigest()
+        out.update({'name' : pathsplit[1]})
+        out.update({'path' : pathsplit[0]})
+        out.update({'sha1' : file_sha1.hexdigest()})
+
+    return out
 
 
+load_dotenv()
 
-
-
+VT_API_KEY = os.getenv('VT_API')  
 
 main()
