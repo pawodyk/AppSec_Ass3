8d745666ba600ac4ef37a26e82ec94928125076c
pwodyk <14218944+pwodyk@users.noreply.github.com>
2022-01-08 21:33:35 +0000

    added working function to upload the file to VT database for analysis, added function to log data to file, and created location for log file location

--- __main__.py
+++ __main__.py
@@ -6,9 +6,17 @@ import json
 import time
 
 def main():
+
+    #print(LOG_DIR)
+
     scanFiles("D:\\Desktop\\Code\\MalFileDetTool\\sample") ## this will be supplied by the user.
-    #print(checkKnownFilesDB('0000004DA6391F7F5D2F7FCCF36CEBDA60C6EA02'))
-    #print(checkKnownFilesDB('40ce9b61ba37022e1b3431b8ee1b37b6eb2f87ac'))
+
+    ### used for testing:
+    # print(checkKnownFilesDB('0000004DA6391F7F5D2F7FCCF36CEBDA60C6EA02'))
+    # print(checkKnownFilesDB('40ce9b61ba37022e1b3431b8ee1b37b6eb2f87ac'))
+
+    # print(checkFileInVT('D:\\Desktop\\Code\\MalFileDetTool\\test\\cmd.exe'))
+    # uploadFileToVT('D:\\Desktop\\Code\\MalFileDetTool\\test\\cmd.exe')
 
     pass
     
@@ -39,7 +47,7 @@ def scanFiles(dirpath):
             subdir.append(path)
         elif os.path.isfile(path):
             out = readfile(path)
-            print('checking file: ',out['name'],out['sha1'])
+            print('checking file:\t',out['name'],out['sha1'])
             file_check = checkKnownFilesDB(out['sha1'])
             if file_check is not None:
                 out['known_file_results'] = file_check
@@ -48,17 +56,20 @@ def scanFiles(dirpath):
                 out['VT_result'] = VT_result
                 time.sleep(30)
 
+            # checkFileInVT(out['sha1'])
+            # time.sleep(30)
+            # uploadFileToVT(os.path.join(out['path'],out['name']))
 
             # print(out)
-            with open("output.json", "a") as file_out:
-                json_file = json.dumps(out, indent=4)
-                file_out.write(json_file)
+
+            json_file = json.dumps(out, indent=4)
+
+            logToFile(json_file, 'output.json')
 
     for directory in subdir:
         print('\n*** lookup in', directory, '***')
         scanFiles(directory)
-
-        
+    
 
 def readfile(filepath):
 
@@ -81,10 +92,13 @@ def checkKnownFilesDB(hash):
     # print(len(response_text))
     if len(response_text) > 42:
         response_json = json.JSONDecoder().decode(response_text[41:])
-        # print(response_json)
+        print(response_json)
+
+        logToFile(response.text, "outputFileDB.json")
+        
         return dict(response_json)
     else:
-        #print("not found")
+        print("not found")
         pass
 
 def checkFileInVT(hash):
@@ -92,32 +106,50 @@ def checkFileInVT(hash):
     #VT_API - Get a file report: https://developers.virustotal.com/reference/file-info
     url = "https://www.virustotal.com/api/v3/files/" + hash
     headers = {
-    "Accept": "application/json",
-    "x-apikey": VT_API_KEY
+        "Accept": "application/json",
+        "x-apikey": VT_API_KEY
     }
     response = requests.request("GET", url, headers=headers)
-    # print(response.text)
+    print(response.text)
 
-    # with open("output.json", "a") as file_out:
-    #     file_out.write(response.text)
-
-    ##opens file in the os for inspection
-    #os.startfile('output.json')
+    logToFile(response.text, 'outputVT_hash.json')
 
     json_resposne = response.json()
 
     return json_resposne
 
 
-def uploadFileToVT():
-    pass
+def uploadFileToVT(path):
+    url = "https://www.virustotal.com/api/v3/files"
 
+    payload = ""
+    
+    with open(path, 'rb') as file:
+        payload = {'file': file.read()}
+
+    headers = {
+        "x-apikey": VT_API_KEY
+    }
+
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+
+    logToFile("uploaded to virus total:" + response.text, "outputVT_file.txt")
+    
+    
+
+def logToFile(text, filename):
+    file = os.path.join(LOG_DIR, filename)
+    with open(file, "a") as file_out:
+        file_out.write(text)
 
 load_dotenv()
 VT_API_KEY = os.getenv('VT_API')
 if VT_API_KEY is None:
     print("API Key not retrived from .env pelase enter manually:")
     VT_API_KEY = input()
+
+LOG_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "log")
 
 
 main()
